96659b5047bdd985f895fcd2660aeff4a69441a4
Liutasil890 <156347231+Liutasil890@users.noreply.github.com>
2024-02-07 18:23:56 +0100

    added comments

--- main-sw/Core/Src/HMI/hmi_display.c
+++ main-sw/Core/Src/HMI/hmi_display.c
@@ -12,6 +12,7 @@
 
 Hmi HMI_init(StateMachine* stateMachine, Sensor* sensorExtruder, Sensor* sensorBack)
 {
+	//Initialisierung Display und darstellen des Grundlayouts
 	Hmi hmi;
 	hmi.HmiInformation.stateMachine = stateMachine;
 	hmi.HmiInformation.sensorExtruder = sensorExtruder;
@@ -46,7 +47,9 @@ Hmi HMI_init(StateMachine* stateMachine, Sensor* sensorExtruder, Sensor* sensorB
 	return hmi;
 }
 
-int HMI_checkBlob(Hmi *hmi, TS_TOUCH_DATA_Def myTS_Handle,int updateHMI){
+int HMI_checkBlob(Hmi *hmi, TS_TOUCH_DATA_Def myTS_Handle,int updateHMI)
+{
+	//Anzeige bei erkanntem Blob
 	if(hmi->HmiInformation.stateMachine->getBlobDetected(hmi->HmiInformation.stateMachine)==1)
 	{
 		if(updateHMI ==1){
@@ -59,46 +62,50 @@ int HMI_checkBlob(Hmi *hmi, TS_TOUCH_DATA_Def myTS_Handle,int updateHMI){
 		if(myTS_Handle.isPressed)
 		{
 			if(myTS_Handle.X >=110 && myTS_Handle.X<=210 && myTS_Handle.Y>=70 && myTS_Handle.Y<=140)
-						{
-						hmi->HmiInformation.stateMachine->setBlobDetected(hmi->HmiInformation.stateMachine,0);
-						ILI9341_Fill(COLOR_WHITE);
+			{
+				hmi->HmiInformation.stateMachine->setBlobDetected(hmi->HmiInformation.stateMachine,0);
+				ILI9341_Fill(COLOR_WHITE);
 
-						ILI9341_Fill_Rect(5, 10, 315, 50, COLOR_ORANGE);
-						ILI9341_printText("Sensor1: 0 mm", 50, 25, COLOR_WHITE, COLOR_ORANGE, 2);
+				ILI9341_Fill_Rect(5, 10, 315, 50, COLOR_ORANGE);
+				ILI9341_printText("Sensor1: 0 mm", 50, 25, COLOR_WHITE, COLOR_ORANGE, 2);
 
-						ILI9341_Fill_Rect(5, 60, 315, 100, COLOR_ORANGE);
-						ILI9341_printText("Sensor2 : 0 mm", 50, 75, COLOR_WHITE, COLOR_ORANGE, 2);
+				ILI9341_Fill_Rect(5, 60, 315, 100, COLOR_ORANGE);
+				ILI9341_printText("Sensor2 : 0 mm", 50, 75, COLOR_WHITE, COLOR_ORANGE, 2);
 
-						char buf[20];
-						sprintf(buf, "Ruhemodus");
-						ILI9341_Fill_Rect(5, 110, 315, 150, COLOR_BLUE);
-						ILI9341_printText(buf, 50, 120, COLOR_WHITE, COLOR_BLUE, 2);
+				char buf[20];
+				sprintf(buf, "Ruhemodus");
+				ILI9341_Fill_Rect(5, 110, 315, 150, COLOR_BLUE);
+				ILI9341_printText(buf, 50, 120, COLOR_WHITE, COLOR_BLUE, 2);
 
-						ILI9341_Fill_Rect(30, 160, 70, 200, COLOR_BLUE);
-						ILI9341_printText("UP", 45,  180, COLOR_WHITE, COLOR_BLUE, 1);
+				ILI9341_Fill_Rect(30, 160, 70, 200, COLOR_BLUE);
+				ILI9341_printText("UP", 45,  180, COLOR_WHITE, COLOR_BLUE, 1);
 
-						ILI9341_Fill_Rect(80, 160, 120, 200, COLOR_BLUE);
-						ILI9341_printText("DOWN", 90,  180, COLOR_WHITE, COLOR_BLUE, 1);
+				ILI9341_Fill_Rect(80, 160, 120, 200, COLOR_BLUE);
+				ILI9341_printText("DOWN", 90,  180, COLOR_WHITE, COLOR_BLUE, 1);
 
-						ILI9341_printText("REGELUNG", 195,  170, COLOR_BLACK, COLOR_WHITE, 2);
+				ILI9341_printText("REGELUNG", 195,  170, COLOR_BLACK, COLOR_WHITE, 2);
 
 
-						ILI9341_Fill_Rect(180, 190, 300, 230, COLOR_RED);
-						ILI9341_printText("IDLE", 230,  205, COLOR_WHITE, COLOR_RED, 2);
-						updateHMI = 1;
-						}
+				ILI9341_Fill_Rect(180, 190, 300, 230, COLOR_RED);
+				ILI9341_printText("IDLE", 230,  205, COLOR_WHITE, COLOR_RED, 2);
+				updateHMI = 1;
+			}
 		}
 
 	}
 	return updateHMI;
 }
 
-void HMI_update_soll(Hmi *hmi,Motor *motor, PIDController *pidController, int operation){
+void HMI_update_soll(Hmi *hmi,Motor *motor, PIDController *pidController, int operation)
+{
+	//Eingabe der Sollwerte
 	float Soll = 0.0;
 	char buf[20];
+	//Anzeige im Ruhemodus
 	if(hmi->HmiInformation.stateMachine->getState(hmi->HmiInformation.stateMachine) == STATE_IDLE){
 		sprintf(buf, "Ruhemodus");
 	}
+	//Eingabe Solldurchmesser
 	else if(hmi->HmiInformation.stateMachine->getState(hmi->HmiInformation.stateMachine) == STATE_AUTOMATIC_MODE){
 		Soll = pidController->get_setPoint(pidController);
 		if(operation == 1){
@@ -109,7 +116,9 @@ void HMI_update_soll(Hmi *hmi,Motor *motor, PIDController *pidController, int op
 		}
 		pidController->set_setPoint(pidController,Soll);
 		sprintf(buf, "Soll: %.2f mm", Soll);
-	}else if(hmi->HmiInformation.stateMachine->getState(hmi->HmiInformation.stateMachine) == STATE_MANUAL_CONTROL){
+	}
+	//Eingabe Solldrehzahl
+	else if(hmi->HmiInformation.stateMachine->getState(hmi->HmiInformation.stateMachine) == STATE_MANUAL_CONTROL){
 		Soll = motor->getSpeed(motor);
 		if(operation == 1){
 			Soll = Soll + 10;
@@ -129,6 +138,7 @@ void HMI_update_soll(Hmi *hmi,Motor *motor, PIDController *pidController, int op
 
 void HMI_getTouch(Hmi *hmi, TS_TOUCH_DATA_Def myTS_Handle,Motor *motor, PIDController *pidController)
 {
+	//Funktion wird aufgerufen, wenn Toucheingabe erkannt
 	if(myTS_Handle.isPressed)
 	{
 		if(myTS_Handle.X >=30 && myTS_Handle.X<=70 && myTS_Handle.Y>=160 && myTS_Handle.Y<=200)
@@ -167,6 +177,7 @@ void HMI_getTouch(Hmi *hmi, TS_TOUCH_DATA_Def myTS_Handle,Motor *motor, PIDContr
 
 void HMI_updateDisplaySensor(Hmi *hmi, float extruder, float back)
 {
+	//Anzeige der aktuellen Sensorwerte
 	if(hmi->HmiInformation.stateMachine->getBlobDetected(hmi->HmiInformation.stateMachine) == 1){
 		return;
 	}
